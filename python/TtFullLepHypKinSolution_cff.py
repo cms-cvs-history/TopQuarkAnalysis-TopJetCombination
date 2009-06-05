674fef5cd1e128e9ca96ee992b3a1e5a1c631e0d
Dirk Dammann <sha1-69d6dd543b0d405e6ee646084bf67e930e968dab@cern.ch>
2009-06-05 07:37:19 +0000

    changed default channel to dimuon

--- python/TtFullLepHypKinSolution_cff.py
+++ python/TtFullLepHypKinSolution_cff.py
@@ -8,7 +8,7 @@ import FWCore.ParameterSet.Config as cms
 ## std sequence to perform kinematic fit
 import TopQuarkAnalysis.TopKinFitter.TtFullLepKinSolutionProducer_cfi
 kinSolutionTtFullLepEventHypothesis = TopQuarkAnalysis.TopKinFitter.TtFullLepKinSolutionProducer_cfi.kinSolutionTtFullLepEvent.clone()
-kinSolutionTtFullLepEventHypothesis.emuChannel=True
+kinSolutionTtFullLepEventHypothesis.mumuChannel=True
 
 ## configure kinSolution hypothesis
 from TopQuarkAnalysis.TopJetCombination.TtFullLepHypKinSolution_cfi import *
