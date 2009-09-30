01acb36521e1ac9e13234061ac52d212854e545f
Dirk Dammann <sha1-69d6dd543b0d405e6ee646084bf67e930e968dab@cern.ch>
2009-09-30 14:13:34 +0000

    jet corrections added

--- python/TtFullLepHypKinSolution_cfi.py
+++ python/TtFullLepHypKinSolution_cfi.py
@@ -8,7 +8,8 @@ ttFullLepHypKinSolution = cms.EDProducer("TtFullLepHypKinSolution",
     muons     = cms.InputTag("selectedLayer1Muons"),
     jets      = cms.InputTag("selectedLayer1Jets"),    
     mets      = cms.InputTag("layer1METs"),
-
+    jetCorrectionLevel = cms.string("abs"),
+    
     match          = cms.InputTag("kinSolutionTtFullLepEventHypothesis"),       
     Neutrinos      = cms.InputTag("kinSolutionTtFullLepEventHypothesis","fullLepNeutrinos"),       
     NeutrinoBars   = cms.InputTag("kinSolutionTtFullLepEventHypothesis","fullLepNeutrinoBars"), 
