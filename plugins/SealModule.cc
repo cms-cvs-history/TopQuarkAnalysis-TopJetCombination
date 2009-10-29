c88f0c76ea897344ed7cc5e0b2ac15b08acfa98a
Eike Schlieckau <sha1-fc2fe05d7bf2eff84264ececb7f974ab67c284cc@cern.ch>
2009-10-29 17:03:43 +0000

    the TtFullHadHypGenMatch module is now armed

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -28,3 +28,7 @@ DEFINE_FWK_MODULE(TtSemiLepHypKinFit);
 
 DEFINE_FWK_MODULE(TtFullLepHypGenMatch);
 DEFINE_FWK_MODULE(TtFullLepHypKinSolution);
+
+// define fully hadronic event hypotheses
+#include "TopQuarkAnalysis/TopJetCombination/plugins/TtFullHadHypGenMatch.h"
+DEFINE_FWK_MODULE(TtFullHadHypGenMatch);
